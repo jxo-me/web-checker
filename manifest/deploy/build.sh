643f42ef6097a41c9eef7c55a357fe94456fb66e
Monkey <mr.monkey.job@gmail.com>
2023-10-19 17:51:53 +0800

    update config

--- manifest/deploy/build.sh
+++ manifest/deploy/build.sh
@@ -5,7 +5,7 @@ function build() {
       -v "$(pwd)":/go/src/work \
       -v "$GOPATH/pkg/mod":/go/src/mod \
       -w /go/src/work \
-      golang:1.18-alpine sh -c "go mod tidy && go build -y -o checker cmd/main.go"
+      golang:1.20-alpine sh -c "go mod tidy && go build -o checker cmd/main.go"
 }
 
 build
